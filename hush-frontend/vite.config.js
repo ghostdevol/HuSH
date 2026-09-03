5d5cab21a080a4dca5a88a2913971c5bc13b2629
karma <karmastudiosbeatz@gmail.com>
2026-09-03 07:52:52 -0600

    Fix Vite config

--- hush-frontend/vite.config.js
+++ hush-frontend/vite.config.js
@@ -1,6 +1,6 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [react()],
+})
